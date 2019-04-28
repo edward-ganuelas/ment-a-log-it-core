a36afed13d596ff53db58e937779b2a645906e76
Edward Ganuelas <epganuelas@gmail.com>
2019-04-28 01:25:37 -0400

    first commit for triple-column

---
+++ src/components/TripleColumn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="triple-column container">
+    <div class="row">
+      <div class="col-12 col-md4"></div>
+      <div class="col-12 col-md4"></div>
+      <div class="col-12 col-md4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TripleColumn',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
