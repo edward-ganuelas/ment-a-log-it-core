77bfda35dfd7405c08829e719c333feaeb8a19e3
Edward Ganuelas <epganuelas@gmail.com>
2019-04-28 01:05:16 -0400

    fonts, basic template styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="container">
+      <router-link to="/" class="col">Home</router-link>
+      <router-link to="/about" class="col">About</router-link>
     </div>
     <router-view/>
   </div>
@@ -11,20 +11,24 @@
 <style lang="scss">
 @import "./node_modules/bootstrap/scss/bootstrap-reboot.scss";
 @import "./node_modules/bootstrap/scss/bootstrap-grid.scss";
+@import url('https://fonts.googleapis.com/css?family=Catamaran');
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Catamaran', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 1rem;
 }
 #nav {
+  text-align: left;
   padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-align: center;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #0066ff;
     }
   }
 }
