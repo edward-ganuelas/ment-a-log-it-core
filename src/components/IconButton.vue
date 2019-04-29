ad59bca03d2414cc57cdd0ab66c506a6c873412e
Edward Ganuelas <epganuelas@gmail.com>
2019-04-28 22:24:07 -0400

    add IconButton

---
+++ src/components/IconButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="clickEvent">
+    <i :class="icon"></i>{{text}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'IconButton',
+  props: {
+    icon: String,
+    text: String,
+    clickEvent: Function
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
