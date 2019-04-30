673f2509e15fccec57ab05c8380e2dd87c391ffa
Edward Ganuelas <epganuelas@gmail.com>
2019-04-30 19:51:53 -0400

    first pass unit test, not working

---
+++ tests/unit/IconButton.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import IconButton from '@/components/IconButton.vue'
+
+describe('IconButton.vue', () => {
+  it('renders with Fontawesome Icon', () => {
+    const iconClass = 'fas fa-check'
+    const wrapper = shallowMount(IconButton, {
+      propsData: { icon: iconClass }
+    })
+    const classList = document.querySelector('button i').classList;
+    expect(classList.contains('fas')).to.be.true;
+  })
+})
