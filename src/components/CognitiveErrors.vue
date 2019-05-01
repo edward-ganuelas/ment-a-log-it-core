8b7dcae86a8180192aa67403af3b5bef0aa9a2e4
Edward Ganuelas <epganuelas@gmail.com>
2019-04-30 20:01:30 -0400

    Implement Vue-select

---
+++ src/components/CognitiveErrors.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cognitive-errors">
+    
+  </div>
+</template>
+
+<script>
+import vSelect from 'vue-select'
+export default {
+  name: 'CognitiveErros',
+  components: {
+    vSelect
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "vue-select/src/scss/vue-select.scss";
+</style>
