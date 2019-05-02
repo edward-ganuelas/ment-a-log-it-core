b5c673c66c0b5cf6982f14cf5ca5954e029f3a65
Edward Ganuelas <epganuelas@gmail.com>
2019-05-01 22:12:06 -0400

    add CognitiveError

---
+++ src/components/CognitiveErrors/CognitiveError.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-12">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-10">
+          <p>{{error}}</p>
+        </div>
+        <div class="col-2">
+          <icon-button icon="far fa-times-circle"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconButton from '@/components/IconButton';
+export default {
+  name: 'HelloWorld',
+  components: {
+    IconButton
+  },
+  props: {
+    error : {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
