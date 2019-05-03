1fa8ba18ee4eaa416fe0f4147708ae4cc17182f8
Edward Ganuelas <epganuelas@gmail.com>
2019-05-02 20:29:16 -0400

    styling and layout updates

--- src/components/TripleColumn.vue
+++ src/components/TripleColumn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="triple-column container">
-    <div class="row"><!--Header-->
+    <div class="row header"><!--Header-->
       <div class="col-12 col-md-4">
         <h3>{{$t('copy.automaticNegativeThought')}}</h3>
       </div>
@@ -11,8 +11,17 @@
         <h3>{{$t('copy.rationalAlternativeThought')}}</h3>
       </div>
     </div>
+    <div class="row">
+      <div class="col-2 offset-10">
+        <icon-button icon="fas fa-plus" />
+      </div>
+    </div>
     <div class="row"><!--TODO Insert Columns Here-->
-
+      <div class="col-12 col-md-4"></div>
+      <div class="col-12 col-md-4">
+        <cognitive-errors />
+      </div>
+      <div class="col-12 col-md-4"></div>
     </div>
   </div>
 </template>
@@ -36,5 +45,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.header{
+  border: 1px solid #ebebeb;
+  border-radius: 15px;
+  padding: 0.5rem 0;
+  h3 {
+    margin-bottom: 0;
+  }
+}
 </style>
