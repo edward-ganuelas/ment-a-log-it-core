11740d6c5e55b16f8cc0bec72330fefcfca1514b
Edward Ganuelas <epganuelas@gmail.com>
2019-05-04 12:55:27 -0400

    update vuex

--- src/store.js
+++ src/store.js
@@ -2,20 +2,27 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 import pathify from './pathify';
+import { make } from 'vuex-pathify'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const state = {
+  NegativeThoughts: [],
+  AutomaticNegativeThoughts: [],
+  CognitiveErrors: [],
+  RationalAlternativeThoughts: []
+};
+
+const mutations = make.mutations(state);
+
+const actions = make.actions(state);
+
+const getters = make.getters(state);
 
 export default new Vuex.Store({
-  state: {
-    AutomaticNegativeThoughts: [],
-    CognitiveErrors: [],
-    RationalAlternativeThoughts: []
-  },
-  mutations: {
-    
-  },
-  actions: {
-
-  },
+  state,
+  mutations,
+  actions,
+  getters,
   plugins: [createPersistedState(), pathify.plugin]
 })
