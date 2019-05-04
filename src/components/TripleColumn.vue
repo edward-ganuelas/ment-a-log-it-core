05c7760244b70a58ce11de2896602915620f0ba8
Edward Ganuelas <epganuelas@gmail.com>
2019-05-04 13:12:16 -0400

    Preliminary vuex binding

--- src/components/TripleColumn.vue
+++ src/components/TripleColumn.vue
@@ -1,19 +1,34 @@
 <template>
 <div class="triple-column container">
-  <negative-thought :isFirstEntry="true" />
-  <negative-thought :isFirstEntry="false" />
+  <negative-thought :isFirstEntry="true" v-for="thought in negativeThoughts" v-bind:key="thought" />
 </div>
 </template>
 
 <script>
 import NegativeThought from '@/components/NegativeThought';
+import { sync } from 'vuex-pathify'
 export default {
   name: 'TripleColumn',
   components: {
     NegativeThought
   },
-  props: {},
-  methods: {},
+  computed: {
+    negativeThoughts: sync('NegativeThoughts'),
+  },
+  methods: {
+    createTripleColumnObject(automaticNegativeThought = '', cognitiveErrors = '', rationalAlternativeThoughts = '') {
+      return {
+        automaticNegativeThought,
+        cognitiveErrors,
+        rationalAlternativeThoughts
+      }
+    },
+  },
+  beforeMount() {
+    if (this.negativeThoughts.length <=0 ) {
+      this.negativeThoughts.push(this.createTripleColumnObject());
+    }
+  }
 };
 </script>
 
