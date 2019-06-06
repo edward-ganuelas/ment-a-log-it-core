ec10f0b27bb6984be21bbe7fd3fbe769a0da3e25
Edward Ganuelas <epganuelas@gmail.com>
2019-06-05 21:25:57 -0400

    add vue2-animate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from '@/copy/copy';
+require('vue2-animate/dist/vue2-animate.min.css')
 
 Vue.config.productionTip = false
 
