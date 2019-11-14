6f202c741d86ef38a64d9e4fc836bdcf4932191d
Edward Ganuelas <epganuelas@gmail.com>
2019-11-13 21:57:52 -0500

    add symptoms select component

---
+++ src/components/DepressionChecklist/SymptomsSelect.vue
@@ -0,0 +1,26 @@
+<template>
+    <select v-model="value" @change="$emit('change', value)">
+        <option v-for="choice in choices" :key="choice.value" :value="choice.value">
+            {{choice.label}}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'SymptomsSelect',
+    data() {
+        return {
+            value: 0,
+            choices: Object.freeze([
+                { value: 0, label: 'Not at All' },
+                { value: 1, label: 'Somewhat' },
+                { value: 2, label: 'Moderately'},
+                { value: 3, label: 'A Lot' },
+                { value: 4, label: 'Extremely' }
+            ])
+        }
+        
+    }
+}
+</script>
--- src/views/DepressionChecklist.vue
+++ src/views/DepressionChecklist.vue
@@ -5,13 +5,22 @@
               <p>{{date}}</p>
           </div>
       </div>
+      <div class="row">
+          <div class="col-12">
+              <symptoms-select />
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 import moment from 'moment';
+import SymptomsSelect from '@/components/DepressionChecklist/SymptomsSelect';
 export default {
     name: 'DepressionChecklist',
+    components: {
+        SymptomsSelect
+    },
     computed: {
         date() {
             return moment().format('MMM DD, YYYY');
