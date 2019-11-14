e3d9f725ff61b48784ee85c59aa685f94ed29222
Edward Ganuelas <epganuelas@gmail.com>
2019-11-13 21:36:35 -0500

    added date

--- src/views/DepressionChecklist.vue
+++ src/views/DepressionChecklist.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="depressionChecklist" class="container">
+      <div class="row">
+          <div class="col-12">
+              <p>{{date}}</p>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
 export default {
-    name: 'DepressionChecklist'
+    name: 'DepressionChecklist',
+    computed: {
+        date() {
+            return moment().format('MMM DD, YYYY');
+        }
+    }
+
 }
 </script>
