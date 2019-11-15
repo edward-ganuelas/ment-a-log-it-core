f10d20490c01ecbbae40705d04b65bc7dea11f0b
Edward Ganuelas <epganuelas@gmail.com>
2019-11-14 20:33:10 -0500

    remove double row on suicidal urges

--- src/views/DepressionChecklist/SuicidalUrges.vue
+++ src/views/DepressionChecklist/SuicidalUrges.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="row">
-        <div class="row">
-            <div class="col-12">
-                <h4>Suicidal Urges</h4>
-            </div>
-            <div class="col-12">
-                <p>Do you have any suicidal thoughts?: <symptoms-select @change="mutateTotal" /></p>
-                <p>Do you want to end your life?: <symptoms-select @change="mutateTotal" /></p>
-                <p>Do you have a plan for harming yourself?: <symptoms-select @change="mutateTotal" /></p>
-            </div>
-            <div class="col-12">
-                <p>Total: {{total}}</p>
-                <p>{{convertedTotal}}</p>
-            </div>
+        <div class="col-12">
+            <h4>Suicidal Urges</h4>
+        </div>
+        <div class="col-12">
+            <p>Do you have any suicidal thoughts?: <symptoms-select @change="mutateTotal" /></p>
+            <p>Do you want to end your life?: <symptoms-select @change="mutateTotal" /></p>
+            <p>Do you have a plan for harming yourself?: <symptoms-select @change="mutateTotal" /></p>
+        </div>
+        <div class="col-12">
+            <p>Total: {{total}}</p>
+            <p>{{convertedTotal}}</p>
         </div>
     </div>
 </template>
