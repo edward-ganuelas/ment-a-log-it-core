0f247d7db55cc522ede0cc7467bf48a2923710f7
Edward Ganuelas <epganuelas@gmail.com>
2019-11-14 21:56:27 -0500

    fix indention

--- src/views/DepressionChecklist/ThoughtsAndFeelings.vue
+++ src/views/DepressionChecklist/ThoughtsAndFeelings.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="row">
-         <div class="col-12">
-              <h4>Thoughts and Feelings</h4>
-          </div>
-          <div class="col-12">
-              <p>Feeling sad or down in the dumps: <symptoms-select key-name="feelingSadOrDownInTheDumpsValue" @change="mutateTotal" /></p>
-              <p>Feeling unhappy or blue: <symptoms-select key-name="feelingUnhappyOrBlueValue" @change="mutateTotal" /></p>
-              <p>Crying spells or tearfulness: <symptoms-select key-name="cryingSpellsValue" @change="mutateTotal" /></p>
-              <p>Feeling discouraged: <symptoms-select key-name="feelingDiscouragedValue" @change="mutateTotal" /></p>
-              <p>Feeling hopeless: <symptoms-select key-name="feelingHopelessValue" @change="mutateTotal" /></p>
-              <p>Low self-esteem: <symptoms-select key-name="lowSelfEsteemValue" @change="mutateTotal" /></p>
-              <p>Feeling worthless or inadequate: <symptoms-select key-name="feelingWorthlessOrInadequateValue" @change="mutateTotal" /></p>
-              <p>Guilt or shame: <symptoms-select key-name="guiltOrShameValue" @change="mutateTotal" /></p>
-              <p>Criticizing yourself or blaming yourself: <symptoms-select key-name="criticizingYourselfValue" @change="mutateTotal" /></p>
-              <p>Difficulty making decisions: <symptoms-select key-name="difficultyMakingDecisionsValue" @change="mutateTotal" /></p>
-          </div>
+        <div class="col-12">
+            <h4>Thoughts and Feelings</h4>
+        </div>
+        <div class="col-12">
+            <p>Feeling sad or down in the dumps: <symptoms-select key-name="feelingSadOrDownInTheDumpsValue" @change="mutateTotal" /></p>
+            <p>Feeling unhappy or blue: <symptoms-select key-name="feelingUnhappyOrBlueValue" @change="mutateTotal" /></p>
+            <p>Crying spells or tearfulness: <symptoms-select key-name="cryingSpellsValue" @change="mutateTotal" /></p>
+            <p>Feeling discouraged: <symptoms-select key-name="feelingDiscouragedValue" @change="mutateTotal" /></p>
+            <p>Feeling hopeless: <symptoms-select key-name="feelingHopelessValue" @change="mutateTotal" /></p>
+            <p>Low self-esteem: <symptoms-select key-name="lowSelfEsteemValue" @change="mutateTotal" /></p>
+            <p>Feeling worthless or inadequate: <symptoms-select key-name="feelingWorthlessOrInadequateValue" @change="mutateTotal" /></p>
+            <p>Guilt or shame: <symptoms-select key-name="guiltOrShameValue" @change="mutateTotal" /></p>
+            <p>Criticizing yourself or blaming yourself: <symptoms-select key-name="criticizingYourselfValue" @change="mutateTotal" /></p>
+            <p>Difficulty making decisions: <symptoms-select key-name="difficultyMakingDecisionsValue" @change="mutateTotal" /></p>
+        </div>
     </div>
 </template>
 
