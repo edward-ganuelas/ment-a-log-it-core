ce1a615df2b184aae1cb810c497de0581c5bb99b
Edward Ganuelas <epganuelas@gmail.com>
2019-11-14 21:36:26 -0500

    total for Physical symptoms

--- src/views/DepressionChecklist/PhysicalSymptoms.vue
+++ src/views/DepressionChecklist/PhysicalSymptoms.vue
@@ -4,20 +4,43 @@
             <h4>Physical Symptoms</h4>
         </div>
         <div class="col-12">
-            <p>Feeling tired: <symptoms-select @change="mutateTotal" /></p>
-            <p>Difficulty sleeping or sleeping too much: <symptoms-select @change="mutateTotal" /></p>
-            <p>Decreased or increased appetite: <symptoms-select @change="mutateTotal" /></p>
-            <p>Loss of interest in sex: <symptoms-select @change="mutateTotal" /></p>
-            <p>Worrying about your health: <symptoms-select @change="mutateTotal" /></p>
+            <p>Feeling tired: <symptoms-select key-name="feelingTiredValue" @change="mutateTotal" /></p>
+            <p>Difficulty sleeping or sleeping too much: <symptoms-select key-name="difficultySleepingOrSleepingTooMuchValue" @change="mutateTotal" /></p>
+            <p>Decreased or increased appetite: <symptoms-select key-name="decreasedOrIncreasedAppetiteValue" @change="mutateTotal" /></p>
+            <p>Loss of interest in sex: <symptoms-select key-name="lossOfInterestInSexValue" @change="mutateTotal" /></p>
+            <p>Worrying about your health: <symptoms-select key-name="worryingAboutYourHealthValue" @change="mutateTotal" /></p>
         </div>
     </div>
 </template>
 <script>
+import mutateTotal from '@/mixins/mutateTotal';
 import SymptomsSelect from '@/components/DepressionChecklist/SymptomsSelect';
 export default {
     name: 'PhysicalSymptoms',
+    mixins: [mutateTotal],
     components: {
         SymptomsSelect
+    },
+    data() {
+        return {
+            feelingTiredValue: 0,
+            difficultySleepingOrSleepingTooMuchValue: 0,
+            decreasedOrIncreasedAppetiteValue: 0,
+            lossOfInterestInSexValue: 0,
+            worryingAboutYourHealthValue: 0
+        }
+    },
+    computed: {
+        value() {
+            return this.feelingTiredValue + this.difficultySleepingOrSleepingTooMuchValue +
+                this.decreasedOrIncreasedAppetiteValue + this.lossOfInterestInSexValue +
+                    this.worryingAboutYourHealthValue;
+        }
+    },
+    watch: {
+        value(newValue) {
+            this.$emit('totalUpdate', 'physicalSymptomsTotal', newValue);
+        }
     }
 }
 </script>
