67c0619eaff869519989e0a474b87e37b6b0182f
Edward Ganuelas <epganuelas@gmail.com>
2019-11-15 20:10:57 -0500

    modules

--- src/store.js
+++ src/store.js
@@ -2,24 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 import pathify from './pathify';
-import { make } from 'vuex-pathify'
+import NegativeThoughts from '@/store/NegativeThoughts';
 
 Vue.use(Vuex);
 
-const state = {
-    NegativeThoughts: []
-};
-
-const mutations = make.mutations(state);
-
-const actions = make.actions(state);
-
-const getters = make.getters(state);
-
 export default new Vuex.Store({
-    state,
-    mutations,
-    actions,
-    getters,
+    modules: {
+        NegativeThoughts 
+    },
     plugins: [createPersistedState(), pathify.plugin]
 })
