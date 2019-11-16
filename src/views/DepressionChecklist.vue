55b3eabc9d2090fe9009813e209e586bf2d184e5
Edward Ganuelas <epganuelas@gmail.com>
2019-11-15 19:37:40 -0500

    header and intro copy

--- src/views/DepressionChecklist.vue
+++ src/views/DepressionChecklist.vue
@@ -2,6 +2,12 @@
     <div id="depressionChecklist" class="container">
         <div class="row">
             <div class="col-12">
+                <h2>{{$t('depressionChecklist.header')}}</h2>
+                <p v-html="$t('depressionChecklist.intro')"></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
                 <p>{{date}}</p>
             </div>
         </div>
