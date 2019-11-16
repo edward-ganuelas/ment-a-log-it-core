109bd48fe576113993c4da86d41de5e2adf91941
Edward Ganuelas <epganuelas@gmail.com>
2019-11-16 17:51:08 -0500

    add depression checklist model

---
+++ src/models/DepressionChecklist.js
@@ -0,0 +1,20 @@
+
+import uuidv4 from 'uuid/v4';
+
+class DepressionChecklist {
+    constructor(date, data) {
+        this.id = uuidv4();
+        this.date = date;
+        this.depressionChecklistData = data;
+    }
+
+    get depressionChecklist() {
+        return {
+            id: this.id,
+            date: this.date,
+            depressionChecklistData: this.depressionChecklistData;
+        }
+    }
+};
+
+export default DepressionChecklist;
