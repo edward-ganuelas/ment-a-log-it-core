87b354a06bededb95f7cb1f3462d555ba68c1e10
Edward Ganuelas <epganuelas@gmail.com>
2019-11-23 17:27:25 -0500

    split up converted total to separate component

---
+++ src/components/DepressionChecklist/ConvertedTotal.vue
@@ -0,0 +1,34 @@
+<template>
+    <p>{{convertedTotal}}</p>
+</template>
+<script>
+export default {
+    name: "ConvertedTotal",
+    props: {
+        total: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        convertedTotal() {
+            const total = this.total;
+            let convertedTotal = '';
+            if (total >= 0 && total <= 5) {
+                convertedTotal = this.$t('depressionChecklist.convertedTotal.noDepression');
+            } else if (total > 5 && total <= 10) {
+                convertedTotal = this.$t('depressionChecklist.convertedTotal.normalButUnhappy');
+            } else if (total > 10 && total <= 25) {
+                convertedTotal = this.$t('depressionChecklist.convertedTotal.mildDepression');
+            } else if (total > 25 && total <= 50) {
+                convertedTotal = this.$t('depressionChecklist.convertedTotal.moderateDepresion');
+            } else if (total > 50 && total <= 75) {
+                convertedTotal = this.$t('depressionChecklist.convertedTotal.severeDepression');
+            } else if (total > 75 ) {
+                convertedTotal = this.$t('depressionChecklist.convertedTotal.extremeDepression');
+            }
+            return convertedTotal;
+        },
+    }
+}
+</script>
--- src/views/DepressionChecklist.vue
+++ src/views/DepressionChecklist.vue
@@ -21,7 +21,7 @@
                     <div class="row">
                         <div class="col-12">
                             <p>Total: {{total}}</p>
-                            <p>{{convertedTotal}}</p>
+                            <converted-total :total="total" />
                             <button @click="save">Save</button>
                         </div>
                     </div>
@@ -42,6 +42,7 @@ import PhysicalSymptoms from '@/views/DepressionChecklist/PhysicalSymptoms';
 import SuicidalUrges from '@/views/DepressionChecklist/SuicidalUrges';
 import DepressionChecklist from '@/models/DepressionChecklist';
 import SavedDepressionChecklist from '@/components/DepressionChecklist/SavedDepressionChecklist';
+import ConvertedTotal from '@/components/DepressionChecklist/ConvertedTotal';
 import { sync } from 'vuex-pathify'
 import persistedStore, { STORE_KEYS } from '@/localforage';
 import moment from 'moment';
@@ -64,7 +65,8 @@ export default {
         ActivitiesAndPersonalRelationships,
         PhysicalSymptoms,
         SuicidalUrges,
-        SavedDepressionChecklist
+        SavedDepressionChecklist,
+        ConvertedTotal
     },
     methods: {
         mutateTotal(key, value) {
@@ -121,24 +123,6 @@ export default {
         },
         total() {
             return this.thoughtsAndFeelingsTotal + this.activitiesAndPersonalRelationshipsTotal + this.physicalSymptomsTotal + this.suicidalUrgesTotal;
-        },
-        convertedTotal() {
-            const total = _.clone(this.total);
-            let convertedTotal = '';
-            if (total >= 0 && total <= 5) {
-                convertedTotal = this.$t('depressionChecklist.convertedTotal.noDepression');
-            } else if (total > 5 && total <= 10) {
-                convertedTotal = this.$t('depressionChecklist.convertedTotal.normalButUnhappy');
-            } else if (total > 10 && total <= 25) {
-                convertedTotal = this.$t('depressionChecklist.convertedTotal.mildDepression');
-            } else if (total > 25 && total <= 50) {
-                convertedTotal = this.$t('depressionChecklist.convertedTotal.moderateDepresion');
-            } else if (total > 50 && total <= 75) {
-                convertedTotal = this.$t('depressionChecklist.convertedTotal.severeDepression');
-            } else if (total > 75 ) {
-                convertedTotal = this.$t('depressionChecklist.convertedTotal.extremeDepression');
-            }
-            return convertedTotal;
         },
         savedDates() {
             if (_.isEmpty(this.savedDepressionCheckList)){
